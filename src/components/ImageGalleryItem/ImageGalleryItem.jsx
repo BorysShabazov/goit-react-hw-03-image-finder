72734c8d8ef84b6871530eaf691fed733b055831
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-08-09 07:07:50 +0200

    fixedOpenImg

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,8 +2,9 @@ import styles from './imageGalleryItem.module.css';
 
 const ImageGalleryItem = ({ webformatURL, openModal, largeImageURL, tag }) => {
   return (
-    <li className={styles.galleryItem} onClick={openModal}>
+    <li className={styles.galleryItem}>
       <img
+        onClick={openModal}
         className={styles.img}
         src={webformatURL}
         alt={tag}
